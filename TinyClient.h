9c06a2441de9e04fce0c0e383bf7f215b5fb2ae6
jujupocacco <julian.mousterou@gmail.com>
2025-07-10 02:58:15 -0400

    Add files via upload

---
+++ TinyClient.h
@@ -0,0 +1,32 @@
+#pragma once 
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h> 
+#include <netinet/tcp.h>
+#include <iostream> 
+#include <stdexcept>
+#include <cstring>
+#include <vector>
+#include <fstream> 
+
+class TinyDBClient{
+	private: 
+		int sockfd; 
+		bool connected;
+
+		int createSocket(); 
+		sockaddr_in serverStructure(); 
+		bool connectionHandling(sockaddr_in serveraddr); 	
+
+	public: 
+		TinyDBClient();
+		~TinyDBClient(); 
+		void TinyClientStart(); 
+		void sendReq(const std::string& req); 
+		std::string recvIntoStr();
+		int recvIntoInt(); 
+		float recvIntoFloat(); 
+		double recvIntoDouble(); 
+		void recvIntoBin(std::string filename); 
+
+};
